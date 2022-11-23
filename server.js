3edffdb6e603902ff9d5e2fc92aee85981785ee8
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2022-11-23 12:32:45 +0100

    Added docker

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ const https = require("https");
 
 const port = process.env.API_PORT || 3000;
 
+const config = {
+  port: port,
+  host: "0.0.0.0",
+};
+
 dbConnection.connect();
 
 if (process.env.NODE_ENV == "development") {
-  app.listen(port, () => {
-    console.log(`App running locally without sslOptions on port ${port}`);
+  app.listen(config.port, config.host, (e) => {
+    if (e) {
+      throw new Error("Error starting server");
+    }
+    console.log(
+      `App running locally without sslOptions on host: ${config.host} : port ${config.port}`
+    );
   });
 } else {
   https.createServer(app).listen(port, () => {
